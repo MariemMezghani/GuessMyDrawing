4885ef00e6245f8a26910af31e73ad876174000f
Mariem Mezghani <mariem.mezghani@hotmail.com>
2023-01-15 10:13:10 +0100

    small fix

--- app/src/main/java/com/github/mariemmezghani/guessmydrawing/ui/DrawingActivity.kt
+++ app/src/main/java/com/github/mariemmezghani/guessmydrawing/ui/DrawingActivity.kt
@@ -1,19 +1,16 @@
 package com.github.mariemmezghani.guessmydrawing.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.github.mariemmezghani.guessmydrawing.R
+import androidx.appcompat.app.AppCompatActivity
 import com.github.mariemmezghani.guessmydrawing.databinding.ActivityDrawingBinding
-import com.github.mariemmezghani.guessmydrawing.databinding.ActivitySetupBinding
 
 class DrawingActivity : AppCompatActivity() {
 
-    private var _binding : ActivityDrawingBinding? =null
-    private val binding : ActivityDrawingBinding
-        get() = _binding!!
+    private lateinit var binding: ActivityDrawingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding= ActivityDrawingBinding.inflate(layoutInflater)
+        binding = ActivityDrawingBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
--- app/src/main/java/com/github/mariemmezghani/guessmydrawing/ui/SetupActivity.kt
+++ app/src/main/java/com/github/mariemmezghani/guessmydrawing/ui/SetupActivity.kt
@@ -1,24 +1,18 @@
 package com.github.mariemmezghani.guessmydrawing.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mariemmezghani.guessmydrawing.databinding.ActivitySetupBinding
 
-
 class SetupActivity : AppCompatActivity() {
 
-
-    private var _binding : ActivitySetupBinding? =null
-    private val binding : ActivitySetupBinding
-        get() = _binding!!
+    private lateinit var binding: ActivitySetupBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
-
-        _binding= ActivitySetupBinding.inflate(layoutInflater)
+        binding = ActivitySetupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
     }
